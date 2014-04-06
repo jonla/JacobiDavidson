de38ce309f0681e009f19254bb494d7932336bcd
David <davlid@student.chalmers.se>
2014-04-06 13:18:45 +0100

    Full orthogonalization in JD. Not tested

--- JD_gminres.m
+++ JD_gminres.m
@@ -3,7 +3,7 @@
 
 
 
-function [lambda, e, res_hist, theta_approximations, count]=JDgminres(A,guess,GmresIterations,tol2,Time)
+function [lambda, e, res_hist, theta_approximations, count]=JD_gminres(A,guess,GmresIterations,tol2,Time)
 
 %%% INITIALIZE:   Sets tolerance levels for convergence,
 %%%              weight for the orthogonality of u to t,
@@ -37,21 +37,21 @@ Ritz_time = 0;
 
 %%%% OUTER LOOP
 for k=1:iterations
-        
+    disp('New iteration beginning')
     
     %%% This part finds eigenpair closest to guess
-%     [eigvec,eigval]=eig(M);      
-%     diff=diag(eigval)-theta_init;
-%     [~, ind] = min(abs(diff));
-%     theta=eigval(ind,ind);
-%     s=eigvec(:,ind)/norm(eigvec(:,ind))
+    %     [eigvec,eigval]=eig(M);
+    %     diff=diag(eigval)-theta_init;
+    %     [~, ind] = min(abs(diff));
+    %     theta=eigval(ind,ind);
+    %     s=eigvec(:,ind)/norm(eigvec(:,ind))
     
     
     %%% Finds largest eigenpair of M
-%     [eigvec,eigval] = eig(M);
-%     [~,indmax] = max(abs(diag(eigval)));
-%     theta = eigval(indmax,indmax);
-%     s = eigvec(:,indmax)/norm(eigvec(:,indmax));
+    %     [eigvec,eigval] = eig(M);
+    %     [~,indmax] = max(abs(diag(eigval)));
+    %     theta = eigval(indmax,indmax);
+    %     s = eigvec(:,indmax)/norm(eigvec(:,indmax));
     
     %%% Finds eigenpar with largest overlapp to v0
     t3 = tic;
@@ -65,12 +65,12 @@ for k=1:iterations
     
     Meig_time = Meig_time + toc(t3);
     M_EIG_TIME = [k Meig_time];
-    
+    disp('M solved for eigenpair')
     t4 = tic;
     [~,indmax] = max(overlap);
     theta = eigval(indmax,indmax);
     s = eigvec(:,indmax)/norm(eigvec(:,indmax));
-        
+    
     
     theta_approximations = [theta_approximations theta];
     
@@ -84,47 +84,31 @@ for k=1:iterations
     RITZ_TIME = [k Ritz_time];
     
     %%% CORRECTION EQUATIONS
+    disp('Solving CEQ...')
     if norm(res) > tol
         
-        % guided correction               
+        % guided correction
+        disp('Guided with initial theta')
+        K = sparse(diag(diag(A))-theta*I);
+        t = dogmres(A,-res,theta,K,u,GmresIterations);
         
-        OCC=((I-u*u')*(A-theta_init*I)*(I-u*u'));
         
         
         
         
-               
     else if r_norm > tol2
-            % orthogonal completion correction
             
-            %%% MLDIVIDE                    
-%             OCC=((I-u*u')*(A-theta*I)*(I-u*u'));
-%             OCC_constrained=[OCC; weight*u'];
-%             res_conc=[-res; 0];
-%             t_mld=mldivide(OCC_constrained,res_conc);
-%             mldNorm = norm(t_mld);
+            disp('Guided with updated theta')
             
-
-            %%% GMRES
-                
-                %t1 = tic;
-                %Ktilde = ((I-u*u')*K*(I-u*u'));
-                %tilde_time = tilde_time + toc(t1)
-                tStart = tic;
-                K = sparse(diag(diag(A))-theta*I);
-                t = dogmres(A,-res,theta,K,u,GmresIterations);
-                %t = gmres(A,-res,[],0.1,5,Ktilde);
-                gmres_time = gmres_time + toc(tStart);
-                GMRES_TIME = [k gmres_time];
-                
-            %%% Comparisons
-                %dogNorm = norm(t);
-                %orthogonal_check=u'*t;
-                %cond_Atilde = cond(OCC)
-                %cond_prec = cond(inv(Ktilde)*OCC)
-                %t=t/norm(t);
-                %t_mld = t/norm(t_mld);
-                %t_overlap = [t_overlap; abs(t_mld'*t)];
+            
+            tStart = tic;
+            K = sparse(diag(diag(A))-theta*I);
+            t = dogmres(A,-res,theta,K,u,GmresIterations);
+            %t = gmres(A,-res,[],0.1,5,Ktilde);
+            gmres_time = gmres_time + toc(tStart);
+            GMRES_TIME = [k gmres_time];
+            
+            
             
         else
             % Returns
@@ -137,45 +121,56 @@ for k=1:iterations
                 GMRES = GMRES_TIME
                 GS = GS_TIME
                 M_BUILD = M_TIME
-            break
-        end        
-    end
-    
-    %%% orthogonalize
-    t1 = tic;
-    for i=1:k 
-        t_prim=t;
-        t=t-V(:,i)'*t*V(:,i);
-        if norm(t)/norm(t_prim) < 0.250
-            for j=1:k
-                t=t-V(:,j)'*t*V(:,j);
+                break
             end
         end
-    end
-    
-    %%% Expands search space
-    v=t/norm(t);
-    V=[V v];
-    gs_time = gs_time + toc(t1);
-    GS_TIME = [k gs_time];
-    
-    
-    
-    %%% Constructs M
-    % This part could be improved by preallocating the size of M
-    t2 = tic;
-    for m=1:k
-        M(m,k+1) = V(:,m)'*A*V(:,k+1);
-        M(k+1,m) = V(:,k+1)'*A*V(:,m);
-    end
-    M(k+1,k+1) = V(:,k+1)'*A*V(:,k+1);
-    M_time = M_time + toc(t2);
-    M_TIME = [k M_time];
-    
-    
-    
-    count=count+1;
-    end
+        disp('Done!')
+        %%% orthogonalize
+        disp('Orthogonalzing...')
+        if mod(k,15) == 0
+            disp('Full reorthogonalization...')
+            for i=1:k-1
+                for j=1:i
+                    V(:,i+1) = V(:,i+1)-V(:,j)'*V(:,i+1)*V(:,j)
+                end
+            end
+        else
+            t1 = tic;
+            for i=1:k
+                t_prim=t;
+                t=t-V(:,i)'*t*V(:,i);
+                if norm(t)/norm(t_prim) < 0.250
+                    for j=1:k
+                        t=t-V(:,j)'*t*V(:,j);
+                    end
+                end
+            end
+        end
+        disp('Done!')
+        %%% Expands search space
+        v=t/norm(t);
+        V=[V v];
+        gs_time = gs_time + toc(t1);
+        GS_TIME = [k gs_time];
         
+        
+        
+        %%% Constructs M
+        % This part could be improved by preallocating the size of M
+        t2 = tic;
+        disp('Building M...')
+        for m=1:k
+            M(m,k+1) = V(:,m)'*A*V(:,k+1);
+            M(k+1,m) = V(:,k+1)'*A*V(:,m);
+        end
+        M(k+1,k+1) = V(:,k+1)'*A*V(:,k+1);
+        M_time = M_time + toc(t2);
+        M_TIME = [k M_time];
+        disp('Done!')
+        
+        
+        count=count+1;
+    end
+    
 end
 
