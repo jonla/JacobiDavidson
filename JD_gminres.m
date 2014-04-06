f7ffa0e764d1717f8c461e957d21b29cc049881d
David <davlid@student.chalmers.se>
2014-04-06 20:22:42 +0100

    Restarting JD

--- JD_gminres.m
+++ JD_gminres.m
@@ -11,8 +11,9 @@ function [lambda, e, res_hist, theta_approximations, count]=JD_gminres(A,guess,G
 %%%               initial eigenvalue approximation and declares
 %%%               variables.
 tol=10000;
+maxit = 5;
 %tol2=10^-6;
-iterations=length(guess);
+dim=length(guess);
 I=sparse(eye(length(guess)));
 v1=guess/norm(guess);
 V=[v1];
@@ -34,99 +35,76 @@ Meig_time = 0;
 Ritz_time = 0;
 
 
-
-%%%% OUTER LOOP
-for k=1:iterations
-    disp('New iteration beginning')
+for m = 1:maxit
     
-    %%% This part finds eigenpair closest to guess
-    %     [eigvec,eigval]=eig(M);
-    %     diff=diag(eigval)-theta_init;
-    %     [~, ind] = min(abs(diff));
-    %     theta=eigval(ind,ind);
-    %     s=eigvec(:,ind)/norm(eigvec(:,ind))
-    
-    
-    %%% Finds largest eigenpair of M
-    %     [eigvec,eigval] = eig(M);
-    %     [~,indmax] = max(abs(diag(eigval)));
-    %     theta = eigval(indmax,indmax);
-    %     s = eigvec(:,indmax)/norm(eigvec(:,indmax));
-    
-    %%% Finds eigenpar with largest overlapp to v0
-    t3 = tic;
-    [eigvec, eigval] = eig(M);
-    
-    overlap = zeros(k,1);
-    for j=1:k
-        overlap(j) = abs(eigvec(1,j));
-    end
-    
-    
-    Meig_time = Meig_time + toc(t3);
-    M_EIG_TIME = [k Meig_time];
-    disp('M solved for eigenpair')
-    t4 = tic;
-    [~,indmax] = max(overlap);
-    theta = eigval(indmax,indmax);
-    s = eigvec(:,indmax)/norm(eigvec(:,indmax));
-    
-    
-    theta_approximations = [theta_approximations theta];
-    
-    %%%% Calculates Ritz pairs and residual vector
-    u=V*s;
-    res=A*u-theta*u;
-    disp('Residual norm:')
-    r_norm = norm(res)
-    res_hist = [res_hist r_norm];
-    Ritz_time = Ritz_time + toc(t4);
-    RITZ_TIME = [k Ritz_time];
-    
-    %%% CORRECTION EQUATIONS
-    disp('Solving CEQ...')
-    if norm(res) > tol
+    %%%% INNER LOOP %%%%
+    for k=1:10
+        disp('New inner iteration beginning')
         
-        % guided correction
-        disp('Guided with initial theta')
-        K = sparse(diag(diag(A))-theta*I);
-        t = dogmres(A,-res,theta,K,u,GmresIterations);
+        %%% Finds eigenpar with largest overlapp to v0
+        t3 = tic;
+        [eigvec, eigval] = eig(M);
         
+        overlap = zeros(k,1);
+        for j=1:k
+            overlap(j) = abs(eigvec(1,j));
+        end
+                
+        Meig_time = Meig_time + toc(t3);
+        M_EIG_TIME = [k Meig_time];
+        disp('M solved for eigenpair')
+        t4 = tic;
+        [~,indmax] = max(overlap);
+        theta = eigval(indmax,indmax);
+        s = eigvec(:,indmax)/norm(eigvec(:,indmax));
+                
+        theta_approximations = [theta_approximations theta];
         
+        %%%% Calculates Ritz pairs and residual vector
+        u=V*s;
+        res=A*u-theta*u;
+        disp('Residual norm:')
+        r_norm = norm(res)
+        res_hist = [res_hist r_norm];
+        Ritz_time = Ritz_time + toc(t4);
+        RITZ_TIME = [k Ritz_time];
         
-        
-        
-    else if r_norm > tol2
-            
-            disp('Guided with updated theta')
-            
-            
-            tStart = tic;
-            Kinv = inv(sparse(sparse(diag(diag(A)))-theta*I));
-            t = dogmres(A,-res,theta,Kinv,u,GmresIterations);
-            %t=Kinv*res;
-            gmres_time = gmres_time + toc(tStart);
-            GMRES_TIME = [k gmres_time];
-            
-            
-            
-            
-        else
-            % Returns
-            lambda=theta;
-            e=u;
-            if strcmp(Time,'T')
-                disp('Timings:')
-                M_EIG_ = M_EIG_TIME
-                RITZ = RITZ_TIME
-                GMRES = GMRES_TIME
-                GS = GS_TIME
-                M_BUILD = M_TIME
-                break
+        %%% CORRECTION EQUATIONS %%%
+        disp('Solving CEQ...')
+        if norm(res) > tol
+                       
+            disp('Guided with initial theta')
+            K = sparse(diag(diag(A))-theta*I);
+            t = dogmres(A,-res,theta,K,u,GmresIterations);
+                       
+        else if r_norm > tol2
+                
+                disp('Guided with updated theta')
+                tStart = tic;
+                Kinv = sparse(1:dim,1:dim,1./(diag(A)-theta*I));
+                t = dogmres(A,-res,theta,Kinv,u,GmresIterations);
+                %t=Kinv*res;
+                gmres_time = gmres_time + toc(tStart);
+                GMRES_TIME = [k gmres_time];
+                
+            else
+                %%% Returns %%%
+                lambda=theta;
+                e=u;
+                if strcmp(Time,'T')
+                    disp('Timings:')
+                    M_EIG_ = M_EIG_TIME
+                    RITZ = RITZ_TIME
+                    GMRES = GMRES_TIME
+                    GS = GS_TIME
+                    M_BUILD = M_TIME
+                    break
+                end
             end
         end
         disp('Done!')
-        %%% orthogonalize
+        
+        %%% orthogonalize %%%
         disp('Orthogonalzing...')
         if mod(k,15) == 0
             disp('Full reorthogonalization...')
@@ -148,58 +126,39 @@ for k=1:iterations
             end
         end
         disp('Done!')
+        
+        
         %%% Expands search space
         v=t/norm(t);
         V=[V v];
         gs_time = gs_time + toc(t1);
         GS_TIME = [k gs_time];
         
-        
+        Vsize = size(V)
         
         %%% Constructs M
         % This part could be improved by preallocating the size of M
         t2 = tic;
         disp('Building M...')
+        W1 = A*(V(:,k+1));
+        W2 = V(:,k+1)'*A;
         for m=1:k
-            M(m,k+1) = V(:,m)'*A*V(:,k+1);
-            M(k+1,m) = V(:,k+1)'*A*V(:,m);
+            M(m,k+1) = V(:,m)'*W1;
+            M(k+1,m) = W2*V(:,m);
         end
-        M(k+1,k+1) = V(:,k+1)'*A*V(:,k+1);
+        M(k+1,k+1) = V(:,k+1)'*W1;
         M_time = M_time + toc(t2);
         M_TIME = [k M_time];
-        disp('Done!')
         
+        Msize = size(M)
         
         count=count+1;
     end
-    
-    
-    %%% Expands search space
-    v=t/norm(t);
-    V=[V v];
-    gs_time = gs_time + toc(t1);
-    GS_TIME = [k gs_time];
-    
-    
-    
-    %%% Constructs M
-    % This part could be improved by preallocating the size of M
-    t2 = tic;
-    W1 = A*(V(:,k+1));
-    W2 = V(:,k+1)'*A;
-    for m=1:k
-        M(m,k+1) = V(:,m)'*W1;
-        M(k+1,m) = W2*V(:,m);
-    end
-    M(k+1,k+1) = V(:,k+1)'*W1;
-    M_time = M_time + toc(t2);
-    M_TIME = [k M_time];
-    
-    
-    
-    count=count+1;
+    %%% Resetting spaces and variables
+    disp('RESTARTING...')
+    M = [theta];
+    V = [u];
 end
-
 
 end
 
