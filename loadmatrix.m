63a9f15557fdd2ec05a3efce08af4208e9959cd7
Jonathan Larsson <jonl@student.chalmers.se>
2014-04-07 18:19:28 +0200

    Adding results from D and E

--- loadmatrix.m
+++ loadmatrix.m
@@ -3,7 +3,7 @@ function [ H ] = loadmatrix( CHAR )
 %   'CHAR' is the matrix name,
 %   ie matrixA, matrixB.
 %   currently 'CHAR' = 'A', 'B',
-%   'C', 'D' or 'F'.
+%   'C', 'D', 'E' or 'F'.
 
 if strcmp(CHAR, 'A') || strcmp(CHAR, 'B')
     dim = 25600
@@ -12,7 +12,12 @@ if strcmp(CHAR, 'C')
     dim = 28224
 end
 if strcmp(CHAR, 'D') || strcmp(CHAR, 'F')
-    dim = 30976;
+    dim = 30976
+end
+if strcmp(CHAR, 'E')
+    dim = 40000
+    load /net/data1/nhqm2014/matrixE.mat H
+    return
 end
 
 path=strcat('/net/data1/nhqm2014/matrix', CHAR);
